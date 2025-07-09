57ebdae058146b51ec23468cd6d02f5cc11de6e0
Mohd Zeeshan Zafar <mohdzeeshan110025@gmail.com>
2025-07-09 18:47:29 +0530

    initial-setup completed

---
+++ exceltodbapi/src/main/java/com/excelapi/ExceltodbapiApplication.java
@@ -0,0 +1,13 @@
+package com.excelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceltodbapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceltodbapiApplication.class, args);
+	}
+
+}
